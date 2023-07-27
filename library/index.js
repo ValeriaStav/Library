39ea48c562644707bcf91d060b6aeece17a2e967
ValeriaStav <valeriiastavriadi@gmail.com>
2023-07-27 23:43:11 +0300

    feat: add index.js

---
+++ library/index.js
@@ -0,0 +1,3 @@
+console.log(
+    "My self-assessment by items:\n\n1. Valid layout +10\n\n2. Semantic layout +16\n\n3. Layout corresponds to layout +54\n\n4. General requirements for layout +20\n\nTotal score: 100 / 100 ;)"
+);
